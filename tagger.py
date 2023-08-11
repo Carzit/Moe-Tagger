478c5fe07c7bc230b2eadd3b19b8c4891d204173
Carzit <126076978+Carzit@users.noreply.github.com>
2023-08-11 17:04:56 +0800

    Add files via upload

--- tagger.py
+++ tagger.py
@@ -18,14 +18,20 @@ with open('checkpoint.json', 'r') as f:
     start_index = checkpoint['stop_at_index'] # 起始图片索引（从第几张图片开始打标）
                                               # 可查看checkpoint.json中保存的最后一次打标的截至索引(stop_at_index)，将其值填入， 从该处继续打标
 
-# 以下代码最好不要动捏
+# 以下代码最后不要动捏
 list_os = os.listdir(dir_path)
-labels = []
+points = []
 
 def mouse_click(event, x, y, flags, para):
-    global labels
+    global points
     if event == cv2.EVENT_LBUTTONDOWN:
-        labels.append((x, y))
+        points.append((x, y))
+        cv2.circle(img_read, (x, y), 5, (0, 255, 0), -1)
+        if len(points) > 1:
+            cv2.line(img_read, points[-2], points[-1], (0, 255, 0), 2)
+        if len(points) == 4:
+            cv2.line(img_read, points[-1], points[0], (0, 255, 0), 2)
+        cv2.imshow('img', img_read)
         print('successfully get point')
 
 def save_checkpoint(index, name):
@@ -57,24 +63,24 @@ if __name__ == '__main__':
             save_checkpoint(image_index, img_name)
             break
         if cv2.waitKey() == ord('p'): # press 'p' to pass (next image)
-            if len(labels) != 4:
+            if len(points) != 4:
                 save_checkpoint(image_index, img_name)
                 raise ValueError(f'''标点数目有误！
-                您在本张图(index:{image_index}, path:{img_name})中共标了{len(labels)}个点，
+                您在本张图(index:{image_index}, path:{img_name})中共标了{len(points)}个点，
                 请标注文字区域四角的四个点！''')
 
             # 向量外积，判断凸四边形
-            v1 = np.array(labels[1]) - np.array(labels[0])
-            v2 = np.array(labels[2]) - np.array(labels[1])
-            v3 = np.array(labels[3]) - np.array(labels[2])
-            v4 = np.array(labels[0]) - np.array(labels[3])
+            v1 = np.array(points[1]) - np.array(points[0])
+            v2 = np.array(points[2]) - np.array(points[1])
+            v3 = np.array(points[3]) - np.array(points[2])
+            v4 = np.array(points[0]) - np.array(points[3])
 
             if ((np.cross(v1, v2) > 0 and np.cross(v2, v3) > 0 and np.cross(v3, v4) > 0 and np.cross(v4, v1) > 0)
                     or (np.cross(v1, v2) < 0 and np.cross(v2, v3) < 0 and np.cross(v3, v4) < 0 and np.cross(v4, v1) < 0)):
-                dic_save = {'p1': labels[0],
-                            'p2': labels[1],
-                            'p3': labels[2],
-                            'p4': labels[3],
+                dic_save = {'p1': points[0],
+                            'p2': points[1],
+                            'p3': points[2],
+                            'p4': points[3],
                             'label': list_os[image_index][:8],
                             'index': image_index}
             else:
@@ -85,7 +91,11 @@ if __name__ == '__main__':
             with open(json_name, 'w') as f:
                 json.dump(dic_save, f)
                 print('successfully saved!')
-            labels.clear()
+
+            points.clear()
             image_index += 1
+            if image_index == len(list_os):
+                print('Thanks! All images have been tagged')
+                break
 
     cv2.destroyAllWindows()
